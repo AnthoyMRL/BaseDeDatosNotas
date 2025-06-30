381a95fcb0247f9251b70da4e7e13fa89bc834a4
AnthonyMRL <anthony.morales.velez@udla.edu.ec>
2025-06-29 21:13:38 -0500

    Creacion del modelo de notas

---
+++ BaseDeDatosNotas/Models/Nota.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseDeDatosNotas.Models
+{
+    [Table("notas")]
+    public class Nota
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [MaxLength(250)]
+        public string Texto { get; set; }
+
+        public DateTime Fecha { get; set; }
+    }
+}
